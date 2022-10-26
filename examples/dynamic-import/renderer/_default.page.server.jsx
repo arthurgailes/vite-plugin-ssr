395bcc7460471dcdcdd429638204097cbe6c21ec
Arthur Gailes <agailes1@gmail.com>
2022-10-26 12:09:30 -0400

    add dynamic-import example

---
+++ examples/dynamic-import/renderer/_default.page.server.jsx
@@ -0,0 +1,32 @@
+export { render }
+export { passToClient }
+import { renderToStream } from 'react-streaming/server'
+import ReactDOMServer from 'react-dom/server'
+import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr'
+import { PageLayout } from './PageLayout'
+
+// See https://vite-plugin-ssr.com/data-fetching
+const passToClient = ['pageProps']
+
+async function render(pageContext) {
+  let pageHtml
+  if (!pageContext.Page) {
+    // SPA
+    pageHtml = ''
+  } else {
+    // SSR / HTML-only
+    const { Page, pageProps } = pageContext
+    pageHtml = await renderToStream(
+      <PageLayout>
+        <Page {...pageProps} />
+      </PageLayout>
+    )
+  }
+
+  return escapeInject`<!DOCTYPE html>
+    <html>
+      <body>
+        <div id="react-container">${pageHtml}</div>
+      </body>
+    </html>`
+}
